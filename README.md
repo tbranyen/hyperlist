38ee51ca35ce796174819f382c6638bbe550e5ef
Tim Branyen <tim@tabdeveloper.com>
2016-04-14 14:29:15 -0700

    Initial commit
    This is a new project: HyperList, that is forked from
    sergi/virtual-list. It provides bug fixes, improvements, a
    standards-compliant ES2015 codebase w/ legacy fallback.
    
    I plan on adding documentation, tests, and maintenance to a stable
    launch.
    
    Test now with: `npm install hyperlist` as I've released it on NPM
    as a

--- README.md
+++ README.md
@@ -1,100 +1,125 @@
-## Virtual DOM List
+## HyperList
 
-This is a simple component that allows the developer to create very
-long lists (by list I mean a single column of rows) that perform extremely
-fast. It does so by loading just the part of the list showing up on the viewport, and by optimizing
-the amount of DOM operations and reflows. It also spends very little memory.
+This is a simple component that can be dropped into any JavaScript application
+and provide a virtual scrolling area that is highly performant and lightweight.
+With zero dependencies and well under 200 lines of code sans comments, it is
+easy to parse and use.
 
-The list could be done even faster by sacrificing the 'momentum' effect, but I
-decided to keep it since it is too big of a sacrifice for the sake of speed.
+## Demo
+
+![Demo](/example.gif?raw=true)
 
 ## Installation
 
-    npm install virtual-list
+```sh
+npm install hyperlist
+```
 
-Or if you prefer bower:
-
-    bower install virtual-list
-
-Of course it can also just be added to any JavaScript project since it consists of a
-single JavaScript file.
+Of course it can also just be added to any JavaScript project since it consists
+of a single JavaScript file.
 
 ## Usage
 
-Each of the following snippets of code creates a virtual list that holds 1 milion
-rows:
+Below is an example of typical usage:
 
-```javascript
-// This will create a scrolling list of 300x300 with 10000 rows. It is necessary to specify
-// how tall each row is by setting the `itemHeight` prpoerty in the config object. In this
-// example, we set up a generator function that will generate each row on demand.
-var list = new ScrollableList({
-  w: 300,
-  h: 300,
-  itemHeight: 31,
-  totalRows: 10000,
-  generatorFn: function(row) {
-    var el = document.createElement("div");
-    el.innerHTML = "ITEM " + row;
-    el.style.borderBottom = "1px solid red";
-    el.style.position = "absolute"
+``` javascript
+// Create a container element or find one that already exists in the DOM.
+const container = document.createElement('div');
+
+// Pass the container element and configuration to the HyperList constructor.
+// You can optionally use the create method if you prefer to avoid `new`.
+const list = HyperList.create(container, {
+  // Default to 100% width. And specify the container to be 300px tall, sets
+  // this as an inline style.
+  height: 300,
+
+  // All items must be the exact same height currently. Although since there is
+  // a generate method, in the future this should be configurable.
+  itemHeight: 30,
+
+  // Specify the total amount of items to render the virtual height.
+  total: 10000,
+
+  // Wire up the data to the index. The index is then mapped to a Y position
+  // in the container.
+  generate(index) {
+    const el = document.createElement('div');
+
+    el.innerHTML = `ITEM ${index}`;
+    el.style.borderBottom = '1px solid red';
+    el.style.position = 'absolute'
+
     return el;
-  }
+  },
 });
-document.body.appendChild(list.container)
 
-// The code below will create an array of 10000 DOM elements beforehand and pass them to
-// the list. The Virtual list will then display them on demand. Of course, even if the
-// virtual list is smart about displaying them, this method fills up a lot of memory by
-// creating the elements before-hand.
-var bigAssList = [];
-for (var i = 0; i < 10000; i++) {
-  var el = document.createElement("div");
-  el.classList.add("item");
-  el.innerHTML = "ITEM " + i;
-  el.style.borderBottom = "1px solid red";
-  bigAssList.push(el);
-}
-
-var list = new ScrollableList({
-  w: 300,
-  h: 300,
-  items: bigAssList,
-  itemHeight: 31
-});
-document.body.appendChild(list.container)
-
-// The code below will create an array of 10000 strings beforehand and pass them to
-// the list. The Virtual list will then display them on demand.
-var bigAssList = [];
-for (var i = 0; i < 10000; i++)
-  bigAssList.push("ITEM " + i);
-
-var list = new ScrollableList({
-  w: 300,
-  h: 300,
-  items: bigAssList,
-  itemHeight: 31
-});
-document.body.appendChild(list.container)
+// Attach the container to the DOM.
+document.body.appendChild(container);
 ```
 
-## Caveats
+An example with all the options, mounted to the entire page that refreshes when
+the browser resizes:
 
-Firefox has a nasty bug (https://bugzilla.mozilla.org/show_bug.cgi?id=373875)
-that breaks any attempt of assigning big numerical values to css properties.
-Since the virtual list does exactly that to give the illusion of a very big list
-without actually loading the components, you might run into that bug for very big
-lists. Unfortunately, I haven't found a way to work around it yet.
+``` javascript
+const config = {
+  width: '100%',
+  height: window.innerHeight,
 
-## License
+  // All items must be the exact same height currently. Although since there is
+  // a generate method, in the future this should be configurable.
+  itemHeight: 30,
 
-The MIT License (MIT)
+  // Specify the total amount of items to render the virtual height.
+  total: 10000,
 
-Copyright (C) 2013 Sergi Mansilla
+  // Reverse the list to start from the bottom instead of the top.
+  reverse: true,
+  
+  // Customize the scroller tag name, defaults to tr.
+  scrollerTagName: 'tr',
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+  // Customize the virtual row class, defaults to vrow.
+  rowClassName: 'vrow',
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  // By default HyperList will determine scroll offset from the container
+  // element. You can override this lookup by using this hook.
+  overrideScrollPosition() {
+    return document.body.scrollTop;
+  },
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  // Wire up the data to the index. The index is then mapped to a Y position
+  // in the container.
+  generate(index) {
+    const el = document.createElement('div');
+
+    el.innerHTML = `ITEM ${index}`;
+    el.style.borderBottom = '1px solid red';
+    el.style.position = 'absolute'
+
+    return el;
+  },
+
+  // Triggerd after any items have been added into the DOM.
+  afterRender() {
+    console.log('Rendered some items');
+  },
+
+  // If you want to do some custom rendering with the container element and
+  // the fragment, you can specify this method. The contents of this function
+  // are the defaults. Look at examples/diffhtml.html for an example of using
+  // this method with a Virtual DOM.
+  applyPatch(element, fragment) {
+    element.innerHTML = '';
+    element.appendChild(fragment);
+  },
+};
+
+// Pass the container element and configuration to the HyperList constructor.
+// You can optionally use the create method if you prefer to avoid `new`.
+const list = HyperList.create(container, config);
+
+window.onresize = () => {
+  config.height = window.innerHeight;
+  list.refresh(container, config);
+};
+```
---
+++ dist/hyperlist.js
@@ -0,0 +1,252 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.HyperList = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+'use strict';
+
+// Default configuration.
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var defaultConfig = {
+  width: '100%',
+  height: '100%'
+};
+
+// Private class properties.
+var _config = Symbol('config');
+var _element = Symbol('element');
+var _scroller = Symbol('scroller');
+var _renderAnimationFrame = Symbol('renderAnimationFrame');
+var _renderChunk = Symbol('renderChunk');
+var _screenItemsLen = Symbol('screenItemsLen');
+var _cachedItemsLen = Symbol('cachedItemsLen');
+var _lastRepaint = Symbol('lastRepaint');
+var _getRow = Symbol('getRow');
+var _getScrollPosition = Symbol('getScrollPosition');
+
+var HyperList = function () {
+  _createClass(HyperList, null, [{
+    key: 'create',
+    value: function create(element, userProvidedConfig) {
+      return new HyperList(element, userProvidedConfig);
+    }
+  }, {
+    key: 'maxElementHeight',
+    get: function get() {
+      var wrapper = document.createElement('div');
+      var fixture = document.createElement('div');
+
+      wrapper.style = 'position: absolute; height: 1px; opacity: 0;';
+      fixture.style = 'height: 1000000000px;';
+
+      wrapper.appendChild(fixture);
+
+      document.body.appendChild(wrapper);
+      var retVal = fixture.offsetHeight;
+      document.body.removeChild(wrapper);
+
+      return retVal;
+    }
+  }]);
+
+  function HyperList(element) {
+    var _this = this;
+
+    var userProvidedConfig = arguments.length <= 1 || arguments[1] === undefined ? defaultConfig : arguments[1];
+
+    _classCallCheck(this, HyperList);
+
+    this[_config] = {};
+    this[_lastRepaint] = 0;
+
+    this.refresh(element, userProvidedConfig);
+
+    var config = this[_config];
+    var context = { scrollTop: 0 };
+
+    if (config.reverse) {
+      requestAnimationFrame(function () {
+        element.scrollTop = (config.total - 1) * config.itemHeight;
+      });
+    }
+
+    // Create internal render loop.
+    var render = function render() {
+      var scrollTop = _this[_getScrollPosition]();
+      var screenItemsLen = _this[_screenItemsLen];
+      var maxBuffer = screenItemsLen * config.itemHeight;
+      var lastRepaint = _this[_lastRepaint];
+
+      _this[_renderAnimationFrame] = requestAnimationFrame(render);
+
+      if (scrollTop === lastRepaint) {
+        return;
+      } else if (!lastRepaint || Math.abs(scrollTop - lastRepaint) > maxBuffer) {
+        _this[_renderChunk]();
+        _this[_lastRepaint] = scrollTop;
+
+        if (typeof config.afterRender === 'function') {
+          config.afterRender();
+        }
+      }
+    };
+
+    render();
+  }
+
+  _createClass(HyperList, [{
+    key: 'destroy',
+    value: function destroy() {
+      cancelAnimationFrame(this[_renderAnimationFrame]);
+    }
+  }, {
+    key: 'refresh',
+    value: function refresh(element, userProvidedConfig) {
+      Object.assign(this[_config], defaultConfig, userProvidedConfig);
+
+      if (!element || element.nodeType !== 1) {
+        throw new Error('HyperList requires a valid DOM Node container');
+      }
+
+      this[_element] = element;
+
+      var config = this[_config];
+
+      var scroller = this[_scroller] || document.createElement(config.scrollerTagName || 'tr');
+
+      if (!config.generate) {
+        throw new Error('Missing required `generate` function');
+      }
+
+      if (Number(config.total) !== Number(config.total)) {
+        throw new Error('Invalid required `total` value, expected number');
+      }
+
+      // Width and height should be coerced to string representations. Either in
+      // `%` or `px`.
+      Object.keys(defaultConfig).filter(function (prop) {
+        return prop in config;
+      }).forEach(function (prop) {
+        var value = config[prop];
+
+        if (typeof value !== 'string' && typeof value !== 'number') {
+          var msg = 'Invalid optional `' + prop + '`, expected string or number';
+          throw new Error(msg);
+        } else if (typeof value === 'number' || value.slice(-1) !== '%') {
+          config[prop] = value + 'px';
+        }
+      });
+
+      // Decorate the container element with inline styles that will match
+      // the user supplied configuration.
+      element.setAttribute('style', '\n      width: ' + config.width + ';\n      height: ' + config.height + ';\n      overflow: auto;\n      position: relative;\n      padding: 0;\n    ');
+
+      var scrollerHeight = config.itemHeight * config.total;
+      var maxElementHeight = HyperList.maxElementHeight;
+
+      if (scrollerHeight > maxElementHeight) {
+        console.warn(['HyperList: The maximum element height ${maxElementHeight}px has', 'been exceeded, please reduce your item height.'].join());
+      }
+
+      scroller.setAttribute('style', '\n      opacity: 0;\n      position: absolute;\n      width: 1px;\n      height: ' + scrollerHeight + 'px;\n    ');
+
+      // Only append the scroller element once.
+      if (!this[_scroller]) {
+        element.appendChild(scroller);
+      }
+
+      var height = userProvidedConfig.height;
+      var elementHeight = element.offsetHeight;
+      var resolvedHeight = typeof height === 'string' ? elementHeight : height;
+
+      this[_screenItemsLen] = Math.ceil(resolvedHeight / config.itemHeight);
+      // Cache 4 times the number of items that fit in the container viewport.
+      this[_cachedItemsLen] = this[_screenItemsLen] * 3;
+
+      // Set the scroller instance.
+      this[_scroller] = scroller;
+
+      // Render after refreshing.
+      this[_renderChunk]();
+    }
+  }, {
+    key: _getRow,
+    value: function value(i) {
+      var config = this[_config];
+      var reverse = config.reverse;
+      var total = config.total;
+      var item = config.generate(i);
+      var itemHeight = config.itemHeight;
+
+      if (!item || item.nodeType !== 1) {
+        throw new Error('Generator did not return a DOM Node for index: ' + i);
+      }
+
+      item.classList.add(config.rowClassName || 'vrow');
+
+      var offsetTop = i * itemHeight;
+      var top = reverse ? (total - 1) * itemHeight - offsetTop : offsetTop;
+
+      item.setAttribute('style', 'position: absolute; top: ' + top + 'px');
+
+      return item;
+    }
+  }, {
+    key: _getScrollPosition,
+    value: function value() {
+      var config = this[_config];
+
+      if (typeof config.overrideScrollPosition === 'function') {
+        return config.overrideScrollPosition();
+      }
+
+      return this[_element].scrollTop;
+    }
+  }, {
+    key: _renderChunk,
+    value: function value() {
+      var config = this[_config];
+      var element = this[_element];
+      var scrollTop = this[_getScrollPosition]();
+      var screenItemsLen = this[_screenItemsLen];
+      var getRow = this[_getRow].bind(this);
+      var total = config.total;
+      var itemHeight = config.itemHeight;
+      var estFrom = Math.floor(scrollTop / itemHeight) - screenItemsLen;
+      var from = estFrom > total ? total : estFrom < 0 ? 0 : estFrom;
+      var estTo = from + this[_cachedItemsLen];
+      var to = estTo > total ? total : estTo < 0 ? 0 : estTo;
+
+      // Append all the new rows in a document fragment that we will later append
+      // to the parent node
+      var fragment = document.createDocumentFragment();
+
+      this[_scroller] = this[_scroller].cloneNode();
+
+      // Keep the scroller in the list of children.
+      fragment.appendChild(this[_scroller]);
+
+      for (var i = from; i < to; i++) {
+        fragment.appendChild(getRow(config.reverse ? config.total - 1 - i : i));
+      }
+
+      if (config.applyPatch) {
+        return config.applyPatch(element, fragment);
+      }
+
+      element.innerHTML = '';
+      element.appendChild(fragment);
+    }
+  }]);
+
+  return HyperList;
+}();
+
+exports.default = HyperList;
+
+},{}]},{},[1])(1)
+});
